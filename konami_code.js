d11bb7d5594cafe0a5587dc1416eb73b49143d44
Annette Michaels <micha232@umn.edu>
2017-10-15 20:01:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var index = 0
 
+document.body.addEventListener(`keydown`, function(e) {keydownHandler(e)})
+
 function init() {
-  document.body.addEventListener(`keydown`, function(e) {keydownHandler(e)})
+
 
   // Write your JavaScript code inside the init() function
   function keydownHandler(e) {
